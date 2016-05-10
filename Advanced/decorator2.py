69483ddef24a7d663ab427dbd813e18be4800d0a
Sneha D L <sneha.dabbadi@gmail.com>
2016-05-10 10:39:07 +0530

    Add files via upload

---
+++ Advanced/decorator2.py
@@ -0,0 +1,138 @@
+# Decorator class
+
+class entry_exit(object):
+
+    def __init__(self, f):
+        self.f = f
+
+    def __call__(self):
+        print("Entering", self.f.__name__)
+        self.f()
+        print("Exited", self.f.__name__)
+
+@entry_exit
+def func1():
+    print("inside func1()")
+
+@entry_exit
+def func2():
+    print("inside func2()")
+
+func1()
+func2()
+
+
+# Decorator Function
+
+def entry_exit(f):
+    def new_f():
+        print("Entering", f.__name__)
+        f()
+        print("Exited", f.__name__)
+    return new_f
+
+@entry_exit
+def func1():
+    print("inside func1()")
+
+@entry_exit
+def func2():
+    print("inside func2()")
+
+func1()
+func2()
+print(func1.__name__)
+
+# Decorator without arguments
+class decorator_without_arguments(object):
+
+    def __init__(self, f):
+        """
+        If there are no decorator arguments, the function
+        to be decorated is passed to the constructor.
+        """
+        print("Inside __init__()")
+        self.f = f
+
+    def __call__(self, *args):
+        """
+        The __call__ method is not called until the
+        decorated function is called.
+        """
+        print("Inside __call__()")
+        self.f(*args)
+        print("After self.f(*args)")
+
+@decorator_without_arguments
+def sayHello(a1, a2, a3, a4):
+    print('sayHello arguments:', a1, a2, a3, a4)
+
+print("After decoration")
+
+print("Preparing to call sayHello()")
+sayHello("say", "hello", "argument", "list")
+print("After first sayHello() call")
+sayHello("a", "different", "set of", "arguments")
+print("After second sayHello() call")
+
+# Decorator with arguments
+class decorator_with_arguments(object):
+
+    def __init__(self, arg1, arg2, arg3):
+        """
+        If there are decorator arguments, the function
+        to be decorated is not passed to the constructor!
+        """
+        print("Inside __init__()")
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
+
+    def __call__(self, f):
+        """
+        If there are decorator arguments, __call__() is only called
+        once, as part of the decoration process! You can only give
+        it a single argument, which is the function object.
+        """
+        print("Inside __call__()")
+        def wrapped_f(*args):
+            print("Inside wrapped_f()")
+            print("Decorator arguments:", self.arg1, self.arg2, self.arg3)
+            f(*args)
+            print("After f(*args)")
+        return wrapped_f
+
+@decorator_with_arguments("hello", "world", 42)
+def sayHello(a1, a2, a3, a4):
+    print('sayHello arguments:', a1, a2, a3, a4)
+
+print("After decoration")
+
+print("Preparing to call sayHello()")
+sayHello("say", "hello", "argument", "list")
+print("after first sayHello() call")
+sayHello("a", "different", "set of", "arguments")
+print("after second sayHello() call")
+
+def decorator_function_with_arguments(arg1, arg2, arg3):
+    def wrap(f):
+        print("Inside wrap()")
+        def wrapped_f(*args):
+            print("Inside wrapped_f()")
+            print("Decorator arguments:", arg1, arg2, arg3)
+            f(*args)
+            print("After f(*args)")
+        return wrapped_f
+    return wrap
+
+@decorator_function_with_arguments("hello", "world", 42)
+def sayHello(a1, a2, a3, a4):
+    print('sayHello arguments:', a1, a2, a3, a4)
+
+print("After decoration")
+
+print("Preparing to call sayHello()")
+sayHello("say", "hello", "argument", "list")
+print("after first sayHello() call")
+sayHello("a", "different", "set of", "arguments")
+print("after second sayHello() call")
